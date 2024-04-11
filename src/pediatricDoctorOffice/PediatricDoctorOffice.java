f178b3068d9166b3552964c97a9abdaa6fd26f79
JkG0112 <166078326+JkG0112@users.noreply.github.com>
2024-04-10 22:25:25 -0700

    Update PediatricDoctorOffice.java

--- src/pediatricDoctorOffice/PediatricDoctorOffice.java
+++ src/pediatricDoctorOffice/PediatricDoctorOffice.java
@@ -1,5 +1,6 @@
 package pediatricDoctorOffice;
 
+// Imports
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -22,53 +23,54 @@ import javafx.scene.text.FontWeight;
 import javafx.scene.control.ToggleGroup;
 import java.util.ArrayList;
 
-
+// Main class that extends Application
 public class PediatricDoctorOffice extends Application {
-	public static final int WIDTH = 700, HEIGHT = 450;
+	public static final int WIDTH = 700, HEIGHT = 450; // Constants for the window dimensions.
 
 	public static void main(String[] args) {
 		launch(args);
 	}
-
+	//Start Method
 	@Override
 	public void start(Stage stage) throws Exception {
-		// TODO Auto-generated method stub
 		StackPane root = new StackPane();
 		Scene scene = new Scene(root, WIDTH, HEIGHT);
-		stage.setTitle("Hospital");
-		String perimeter = " -fx-border-color: black;\r\n" 
-						 + "    -fx-border-insets: 1;\r\n"
-						 + "    -fx-border-width: 2;\r\n";
+		stage.setTitle("Hospital");// Sets the title for the window.
+		
+		String perimeter = " -fx-border-color: black;\r\n"  + "    -fx-border-insets: 1;\r\n"+ "    -fx-border-width: 2;\r\n";
 		Font boldFont = Font.font("Arial", FontWeight.BOLD, 14);
 		
-
-		/*-----LOGIN PAGE------------------------------------------------------------------------------*/
+		 // VBox layout 
 		VBox login = new VBox();
 		login.setAlignment(Pos.CENTER);
-		
+
+		// Buttons for employee and patient login.
 		Button employeeLogin = new Button("Employee"); // made change here
 		Button patientLogin = new Button("Patient Login");
-		
+
+		// Setting the size of the VBox container and the buttons.
 		login.setMinHeight(200);
 		login.setMinWidth(180);
 		employeeLogin.setPrefSize(150, 50);
 		patientLogin.setPrefSize(150, 50);
 		
 		login.getChildren().addAll(employeeLogin, patientLogin);
-		/*---------------------------------------------------------------------------------------------*/
 		
-		employeeLogin.setOnAction(event -> { //when employee login button is clicked
+		// Event handler for the employee login button
+		employeeLogin.setOnAction(event -> { 
 			EmployeeLogin employeeLoginGUI = new EmployeeLogin();
 			stage.close();
 			employeeLoginGUI.start(new Stage());
 		});
-		
+
+		// Event handler for the patient login button.
 		patientLogin.setOnAction(event -> { //when patient login button is clicked
 			PatientLogin patientLoginGUI = new PatientLogin();
 			stage.close();
 			patientLoginGUI.start(new Stage());
 		});
-	
+
+		// Adding the VBox to the root container 
 		root.getChildren().add(login);
 		stage.setScene(scene);
 		stage.setResizable(false);
